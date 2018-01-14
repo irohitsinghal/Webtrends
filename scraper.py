4c38d223734559db7219a9e63c6b303e265bce6c
Rohit Singhal <rohit@localhost>
2018-01-14 11:53:12 +0530

    added opennlp server support

--- scraper.py
+++ scraper.py
@@ -1,11 +1,24 @@
 import requests;
+from jsonrpclib.jsonrpc import ServerProxy
+from pprint import pprint
 
-url = 'https://hacker-news.firebaseio.com/v0/beststories.json'
-req = requests.get(url);
-data = list(req.json());
+class OpenNLP:
+	def __init__(self, host='localhost', port=8080):
+		uri = "http://%s:%d" % (host, port)
+		self.server = ServerProxy(uri)
 
-for i in data:
-	url = 'https://hacker-news.firebaseio.com/v0/item/' + str(i)+'.json';
+	def parse(self, text):
+		return self.server.parse(text)
+
+if __name__ == '__main__':
+	nlp = OpenNLP()
+	url = 'https://hacker-news.firebaseio.com/v0/beststories.json'
 	req = requests.get(url);
-	print(dict(req.json())['title']);
+	data = list(req.json());
+
+	for i in data:
+		url = 'https://hacker-news.firebaseio.com/v0/item/' + str(i)+'.json';
+		req = requests.get(url);
+		refined = nlp.parse(dict(req.json())['title']);
+		print(refined);
 	
